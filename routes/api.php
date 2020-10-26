5cb005717d82ff391511464719110d6c00ce793c
liujianwei <liu8jianwei@163.com>
2020-10-26 22:55:33 +0800

    ddd

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('queue', 'Queue@index');
+Route::get('index', 'Queue@insert');

5d6f1da88dfe855ad6d1a75f6951125b871ef21b
“liu8jianwei” <liu8jianwei@163.com>
2020-10-29 17:04:16 +0800

    mq

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,12 +1,12 @@
 <?php return array (
   'root' => 
   array (
-    'pretty_version' => '1.0.0+no-version-set',
-    'version' => '1.0.0.0',
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
     'aliases' => 
     array (
     ),
-    'reference' => NULL,
+    'reference' => '95b938744f41384009df97b95bcb065c13796ff7',
     'name' => 'laravel/laravel',
   ),
   'versions' => 
@@ -440,12 +440,12 @@
     ),
     'laravel/laravel' => 
     array (
-      'pretty_version' => '1.0.0+no-version-set',
-      'version' => '1.0.0.0',
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
       'aliases' => 
       array (
       ),
-      'reference' => NULL,
+      'reference' => '95b938744f41384009df97b95bcb065c13796ff7',
     ),
     'laravel/tinker' => 
     array (
